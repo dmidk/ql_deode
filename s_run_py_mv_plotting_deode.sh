8aa6a8f1cedfffc60cce001fc8053a376b151cd8
Fabrizio Baordo <fab@dmi.dk>
2023-10-20 12:07:17 +0000

    added more shortname variables in py_mv_config.yml, a bit more description in the README

--- s_run_py_mv_plotting_deode.sh
+++ s_run_py_mv_plotting_deode.sh
@@ -2,12 +2,12 @@
 
 # change here to submit with sbatch
 # mem > 8000 can be necessary for high reslution domains
-#SBATCH --job-name=py_mv_plotting
-#SBATCH --qos=nf
-#SBATCH --time=00:30:00
-#SBATCH --nodes=1
-#SBATCH --ntasks=1
-#SBATCH --mem=12000
+##SBATCH --job-name=py_mv_plotting
+##SBATCH --qos=nf
+##SBATCH --time=00:30:00
+##SBATCH --nodes=1
+##SBATCH --ntasks=1
+##SBATCH --mem=12000
 
 module load python3/3.8.8-01           #3.10.10-01
 module load ecmwf-toolbox/2021.08.3.0  #2023.04.1.0
@@ -19,16 +19,16 @@ ecType=none
 
 # userId: needed to identify location of grib files
 # your user id on atos; otherwise specify other user id
-userId=nhe
+userId=miag
 
 # List name of experiment and the corresponding domain 
 # necessary to pick up location and grib file name
 
 #expNames=(arome_2500 arome_1500 arome_750 harmonie_DK_2500 harmonie_DK_1500 harmonie_DK_750)
-expNames=(harmonie_DK_500 harmonie_DK_500_SP) #harmonie_DK_500 
+expNames=(DK1500_20190614)
 
 #domNames=(DK_300x300_2500m DK_500x500_1500m DK_1000x1000_750m DK_300x300_2500m DK_500x500_1500m DK_1000x1000_750m)
-domNames=(DK_1500x1500_500m DK_1500x1500_500m)
+domNames=(DK_500x500_1500m)
 
 
 # List of AN times
@@ -54,25 +54,25 @@ for (( i=0; i<${nE}; i++ ));
   domID=${domNames[$i]}
 
 # Set --> YEAR
-for YYYY in {2017..2017}; do
+for YYYY in {2019..2019}; do
  # Set --> MONTH
- for MM in {09..09}; do
+ for MM in {06..06}; do
   # Set --> DAY
-  for DD in {17..17}; do 
+  for DD in {14..14}; do 
   # Loop over AN times
   for (( j=0; j<${nHH}; j++ ));
     do
      anHH=${hours[$j]}
       if [[ ${anHH} == "06" ]] || [[ ${anHH} == "18" ]] ; then
         # Set --> FC STEP 
-        for fcHHH in {008..008}; do 
+        for fcHHH in {012..020}; do 
           python3 ${myPyFile} $YYYY $MM $DD $anHH $fcHHH $expID $ecType $domID $userId $myYmlFile
         done
       fi
 
       if [[ ${anHH} == "00" ]] || [[ ${anHH} == "12" ]] ; then
         # Set --> FC STEP    
-        for fcHHH in {012..012}; do 
+        for fcHHH in {012..020}; do 
           python3 ${myPyFile} $YYYY $MM $DD $anHH $fcHHH $expID $ecType $domID $userId $myYmlFile
         done
       fi
