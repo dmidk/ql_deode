6044b9b2ba7b2e118b0fa3b97c6509afed80f282
ealerskans <ea@dmi.dk>
2024-01-02 07:26:48 +0000

    Add date variables for more flexible looping

--- s_run_py_mv_plotting_deode.sh
+++ s_run_py_mv_plotting_deode.sh
@@ -4,10 +4,10 @@
 # mem > 8000 can be necessary for high reslution domains
 #SBATCH --job-name=py_mv_plotting
 #SBATCH --qos=nf
-#SBATCH --time=00:30:00
+#SBATCH --time=01:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --mem=12000
+#SBATCH --mem=16000
 
 module load python3/3.8.8-01           #3.10.10-01
 module load ecmwf-toolbox/2021.08.3.0  #2023.04.1.0
@@ -19,20 +19,30 @@ ecType=none
 
 # userId: needed to identify location of grib files
 # your user id on atos; otherwise specify other user id
-userId=nhe
+userId=nhac
 
 # List name of experiment and the corresponding domain 
 # necessary to pick up location and grib file name
 
-#expNames=(arome_2500 arome_1500 arome_750 harmonie_DK_2500 harmonie_DK_1500 harmonie_DK_750)
-expNames=(harmonie_DK_500 harmonie_DK_500_SP) #harmonie_DK_500 
-
-#domNames=(DK_300x300_2500m DK_500x500_1500m DK_1000x1000_750m DK_300x300_2500m DK_500x500_1500m DK_1000x1000_750m)
-domNames=(DK_1500x1500_500m DK_1500x1500_500m)
+domNames=(DK_300x300_2500m DK_500x500_1500m DK_1000x1000_750m DK_1500x1500_500m)
+expNames=(DK_2500m_20210708_harmonie DK_1500m_20210708_harmonie DK_750m_20210708_harmonie DK_500m_20210708_harmonie)
 
 
 # List of AN times
 hours=(00) 
+# Start and end year
+year1=2021
+year2=2021
+# Start and end month
+month1=7
+month2=7
+# Start and end day
+day1=8
+day2=8
+# Start and end fc length
+fc1=15
+fc2=22
+
 
 # --> Adjust dates/time (YEAR, MONTH, DAY, FC STEP) in loops below 
 
@@ -54,25 +64,25 @@ for (( i=0; i<${nE}; i++ ));
   domID=${domNames[$i]}
 
 # Set --> YEAR
-for YYYY in {2017..2017}; do
+for YYYY in $(seq -f "%04g" $year1 $year2); do
  # Set --> MONTH
- for MM in {09..09}; do
+ for MM in $(seq -f "%02g" $month1 $month2); do
   # Set --> DAY
-  for DD in {17..17}; do 
+  for DD in $(seq -f "%02g" $day1 $day2); do
   # Loop over AN times
   for (( j=0; j<${nHH}; j++ ));
     do
      anHH=${hours[$j]}
       if [[ ${anHH} == "06" ]] || [[ ${anHH} == "18" ]] ; then
         # Set --> FC STEP 
-        for fcHHH in {008..008}; do 
+        for fcHHH in $(seq -f "%04g" $fc1 $fc2); do
           python3 ${myPyFile} $YYYY $MM $DD $anHH $fcHHH $expID $ecType $domID $userId $myYmlFile
         done
       fi
 
       if [[ ${anHH} == "00" ]] || [[ ${anHH} == "12" ]] ; then
         # Set --> FC STEP    
-        for fcHHH in {012..012}; do 
+        for fcHHH in $(seq -f "%04g" $fc1 $fc2); do
           python3 ${myPyFile} $YYYY $MM $DD $anHH $fcHHH $expID $ecType $domID $userId $myYmlFile
         done
       fi
