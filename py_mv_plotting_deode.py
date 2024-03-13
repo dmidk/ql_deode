f6094cc2248ae9dc7cf3f92d280c37a96e59cd63
Fabrizio Baordo <fab@dmi.dk>
2024-03-13 09:28:47 +0100

    Update py_mv_plotting_deode.py
    wgst renamed to 10mwgst

--- py_mv_plotting_deode.py
+++ py_mv_plotting_deode.py
@@ -194,7 +194,7 @@ for shortName in varList:
             var = mv.direction(u_10m, v_10m)
             var = mv.grib_set_long(var, ['paramId', 3031])
         # calculate 10m wind gust speed        
-        elif shortName == 'wgst':
+        elif shortName == '10mwgst':
             ugst_10m =  myFC.select(shortName='10efg', typeOfLevel=typeOfLevel, level=level, stepRange=FC_step_1+'-'+FC_step)
             vgst_10m =  myFC.select(shortName='10nfg', typeOfLevel=typeOfLevel, level=level, stepRange=FC_step_1+'-'+FC_step)
             var = mv.sqrt(ugst_10m*ugst_10m + vgst_10m*vgst_10m)
